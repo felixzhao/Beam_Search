bde9445ed9c443aff5d38250095ba7667b2a0513
felixzhao <felixz2010@gmail.com>
2013-10-30 17:51:53 +0800

    new pseudo code, seems work fine. idea is about generate temp replement set.

---
+++ beam_search_5.py
@@ -0,0 +1,30 @@
+unks = []
+cands = []
+docs = []
+results = {}
+
+def add2result(unk, cands):
+    result[unk] = cands
+
+def generatedoc(cand):
+  
+
+def beam_search():
+  if len(cands) != 0:
+    add2result(unks.top,cands) # top unks, all cands
+    unks.remove(top)
+    docs.clean()
+    for cand in cands:
+      docs.add(generatedoc(cand))
+    cands.clean()
+    if len(unks) == 0:
+      return
+    else
+      unk = unks.top
+      tempcands = [(,)]
+      for doc in docs:
+        tempcands.append((findcands(unk, doc),doc)) # generate tuple (cand,doc)
+      tempcands = tempcands.sort().top(n)
+      docs = tempcands[doc] # just keep the doc which find cands
+      cands = tempcands[cand] # put cands info to cands Queue without doc info
+      
